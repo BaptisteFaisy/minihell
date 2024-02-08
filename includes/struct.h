c88f83ce23edcf2b4d0c44b6cc407fe1ca991cbe
Hojoon LEE <joonlee031@live.com>
2024-02-08 17:56:54 +0100

    hh

--- includes/struct.h
+++ includes/struct.h
@@ -6,7 +6,7 @@
 /*   By: lhojoon <lhojoon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/08 09:47:55 by lhojoon           #+#    #+#             */
-/*   Updated: 2024/02/08 17:25:17 by bfaisy           ###   ########.fr       */
+/*   Updated: 2024/02/08 17:37:24 by lhojoon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,22 +20,26 @@
  * Fill up this structure from parsing part
  * Use this structure from execution part
  * 
- * @param cmd - command
- * @param args - list of arguments
+ * @param cmd - command, definitivly not NULL
+ * @param args - list of arguments, NULLABLE
  * @param opts - list of options without hyphen (-), sorted by input order
- * @param red_in - input redirection file
- * 
+ * @param red_in - input redirection file (<), NULLABLE
+ * @param red_out - output redirection file (>), NULLABLE
+ * @param red_in_delim - input redirection delimiter (<<), NULLABLE
+ * @param red_out_append - output redirection file, append mode (>>), NULLABLE
+ * @param envp - environment variables (from main)
+ * @param next - next command pack, NULLABLE (BY REQUEST OF BFAISY ^^)
  */
 typedef struct s_cmd_args
 {
-	char		*cmd;
-	t_list		*args;
-	t_list		*opts;
-	t_list		*red_in; // mettre le fichier dedans
-	t_list		*red_out; // mettre le fichier dedans
-	t_list		*red_in_delim; // mettre la chose apres les << sinon NULL
-	t_list		*red_out_append; // mettre le fichier dedans sinon NULL
-	char		**envp;
+	char				*cmd;
+	t_list				*args;
+	t_list				*opts;
+	t_list				*red_in;
+	t_list				*red_out;
+	t_list				*red_in_delim;
+	t_list				*red_out_append;
+	char				**envp;
 	struct s_cmd_args	*next;
 }	t_cmd_args;
 
--- srcs/execution_main.c
+++ srcs/execution_main.c
@@ -1,32 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   execution_main.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lhojoon <lhojoon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/07 14:45:12 by bfaisy            #+#    #+#             */
-/*   Updated: 2024/02/08 17:40:09 by lhojoon          ###   ########.fr       */
+/*   Created: 2024/02/08 17:37:49 by lhojoon           #+#    #+#             */
+/*   Updated: 2024/02/08 17:48:16 by lhojoon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	main(int ac, char **av, char **ev)
+int	execution(t_cmd_args *cmd_args)
 {
-	char	*str;
-	(void)ac ;(void)av; (void)ev;
-	while (1)
-	{
-		write(1, "$> ", 3);
-		str = get_next_line(0);
-		parsing(str);
-		if (ft_strncmp(str, "exit", 4) == 0)
-		{
-			free(str);
-			return (0);
-		}
-		free(str);
-	}
+	(void)cmd_args;
 	return (0);
 }
