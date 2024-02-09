be3786c73a8b45795ac3337fb38da4435b05540e
HADMARINE <joonlee031@live.com>
2024-02-09 17:33:51 +0100

    Update param struct

--- includes/struct.h
+++ includes/struct.h
@@ -6,7 +6,7 @@
 /*   By: lhojoon <lhojoon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/08 09:47:55 by lhojoon           #+#    #+#             */
-/*   Updated: 2024/02/08 18:36:12 by bfaisy           ###   ########.fr       */
+/*   Updated: 2024/02/09 17:33:41 by lhojoon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,22 +15,16 @@
 
 # include "libft.h"
 
+
 /**
- * @brief Structure for command args
- * Fill up this structure from parsing part
- * Use this structure from execution part
+ * @brief Structure for redirections 
  * 
- * @param cmd - command, definitivly not NULL
- * @param args - list of arguments, NULLABLE
- * @param opts - list of options without hyphen (-), sorted by input order
  * @param red_in - input redirection file (<), NULLABLE
  * @param red_out - output redirection file (>), NULLABLE
  * @param red_in_delim - input redirection delimiter (<<), NULLABLE
  * @param red_out_append - output redirection file, append mode (>>), NULLABLE
- * @param envp - environment variables (from main)
- * @param next - next command pack, NULLABLE (BY REQUEST OF BFAISY ^^)
- */
 
+*/
 typedef struct s_red
 {
 	t_list				*red_in; // mettre le fichier dedans
@@ -38,6 +32,19 @@ typedef struct s_red
 	t_list				*red_in_delim; // mettre la chose apres les << sinon NULL
 	t_list				*red_out_append; // mettre le fichier dedans sinon NULL
 }	t_red;
+
+/**
+ * @brief Structure for command args
+ * Fill up this structure from parsing part
+ * Use this structure from execution part
+ * 
+ * @param cmd - command, NULLABLE
+ * @param args - list of arguments, NULLABLE
+ * @param opts - list of options without hyphen (-), sorted by input order
+ * @param redirect - redirections
+ * @param envp - environment variables (from main)
+ * @param next - next command pack, NULLABLE (BY REQUEST OF BFAISY ^^)
+ */
 
 typedef struct s_cmd_args
 {
